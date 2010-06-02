f8dd55cbc92ae6e4be7305916045f59ef38c34f2
tpapp <tkpapp@gmail.com>
2010-06-02 16:19:48 +0200

    Removed XARRAY, NAMED-READTABLES.

--- unit-tests/package.lisp
+++ unit-tests/package.lisp
@@ -1,7 +1,6 @@
 (defpackage #:cl-random-unit-tests
-    (:use #:cl #:cl-utilities #:iterate #:metabang-bind #:cffi #:xarray
-          #:anaphora #:named-readtables #:lift #:lla #:cl-random
-          #:cl-num-utils)
+    (:use #:cl #:cl-utilities #:iterate #:metabang-bind #:anaphora #:lift #:lla
+          #:cl-random #:cl-num-utils)
   (:shadowing-import-from :iterate :collecting :collect)
   (:shadowing-import-from :cl-random #:type)
 ;  (:import-from cl-random)
