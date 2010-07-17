2f4d830c3a49035f117cb0b462928b48b342d5eb
tpapp <tkpapp@gmail.com>
2010-07-17 16:54:32 +0200

    Added dummy priors for LINEAR-REGRESSION.

--- src/regressions.lisp
+++ src/regressions.lisp
@@ -2,17 +2,26 @@
 
 (in-package #:cl-random)
 
-(defgeneric dummy-observations (object &key &allow-other-keys)
-  (:documentation "Return dummy observations corresponding to a regression, or a
-  random variable representing draws from a regression's posterior.  Return
-  (values Y X)."))
+;;; Dummies from priors
+;;; 
+;;; Convert conjugate priors to dummy observations.
 
-(defmethod dummy-observations ((rv mv-normal) &key)
-  (bind (((:slots-r/o mean variance-right-sqrt) rv)
-         (r-t (transpose variance-right-sqrt)))
-    (values (solve r-t mean)
-            (invert r-t))))
+(defgeneric dummy-observations (prior &key &allow-other-keys)
+  (:documentation "Return dummy observations (VALUES Y X) corresponding to a
+  regression, or a random variable representing draws from a regression's
+  posterior."))
 
+(defun add-prior-dummies (y x prior &optional prior-type)
+  "When PRIOR is non-nil, add dummies to Y and X, otherwise make no changes.
+Return (VALUES Y X).  When PRIOR-TYPE is given, check that PRIOR is of that
+type."
+  (when (and prior prior-type)
+    (assert (typep prior prior-type)))
+  (if prior
+      (bind (((:values y-dummy x-dummy) (dummy-observations prior)))
+        (values (concat y y-dummy)
+                (stack-vertically x x-dummy)))
+      (values y x)))
 
 ;;; LINEAR-REGRESSION-KV
 ;;; 
@@ -20,10 +29,10 @@
 ;;; practice, but useful for Gibbs sampling.
 
 (defun linear-regression-kv (y x &key variance-right-sqrt prior)
-  "Linear regression of Y on X with known variance matrix.  VARIANCE-RIGHT-SQRT
-is the rigth square root of the variance matrix, assumed to be the identity if
-not given.  PRIOR is used to generate dummy observations with the eponymous
-function."
+  "Linear regression of Y on X with known variance matrix, returned as an
+MV-NORMAL distribution.  VARIANCE-RIGHT-SQRT is the rigth square root of the
+variance matrix, assumed to be the identity if not given.  PRIOR is used to
+generate dummy observations with the eponymous function."
   (check-type y vector)
   (check-type x dense-matrix-like)
   ;; apply variance matrix if given
@@ -33,14 +42,16 @@ function."
     (setf y (solve variance-right-sqrt y)))
   ;; attach prior to transformed data; dummy observations don't need to be
   ;; rescaled as they come directly from the prior
-  (when prior
-    (bind (((:values y-dummy x-dummy) (dummy-observations prior)))
-      (setf x (stack-vertically x x-dummy)
-            y (concat y y-dummy))))
-  (bind (((:values beta nil nil other-values) (least-squares y x :method :qr)))
+  (bind (((:values y x) (add-prior-dummies y x prior 'mv-normal))
+         ((:values beta nil nil other-values) (least-squares y x :method :qr)))
     (make-instance 'mv-normal :mean beta :variance-right-sqrt
                    (qr-xx-inverse-sqrt (getf other-values :qr)))))
 
+(defmethod dummy-observations ((rv mv-normal) &key)
+  (bind (((:slots-r/o mean variance-right-sqrt) rv)
+         (r-t (transpose variance-right-sqrt)))
+    (values (solve r-t mean)
+            (invert r-t))))
 
 ;;;  LINEAR-REGRESSION
 ;;;
@@ -70,13 +81,26 @@ when inverting the singular values."
           (funcall mv-normal-generator (sqrt scaling-factor))
           (* scaling-factor s^2))))))
 
-(defun linear-regression (y x &key r^2? (method :qr))
+(defmethod dummy-observations ((rv linear-regression) &key)
+  (bind (((:accessors-r/o mv-normal s^2 nu) rv)
+         (nu (as-integer nu))
+         (s (sqrt s^2))
+         ((:slots-r/o mean variance-right-sqrt) mv-normal)
+         (k (length mean))
+         (r-t (transpose (e/ variance-right-sqrt s))))
+    (values (concat (solve r-t mean) (lla-vector nu :double s))
+            (stack-vertically (invert r-t) 
+                              (make-matrix nu k :double
+                                           :initial-element 0d0)))))
+
+(defun linear-regression (y x &key r^2? (method :qr) prior)
   "Return the following values: 1. an MV-T distribution for drawing
   means from the distribution.  2. The mean of the variance posterior.
   Multiplied by the second value returned when drawing from the MV-T
   distribution, this yields the variance corresponding to that draw.
   3. When R^2?, return the R^2 value."
-  (bind (((:values b ss nu other-values) (least-squares y x :method method))
+  (bind (((:values y x) (add-prior-dummies y x prior 'linear-regression))
+         ((:values b ss nu other-values) (least-squares y x :method method))
          (s^2 (/ ss nu)))
     (aprog1
         (make-instance 'linear-regression
--- unit-tests/regression-tests.lisp
+++ unit-tests/regression-tests.lisp
@@ -6,11 +6,22 @@
   ()
   (:equality-test #'lla=))
 
+;;; utility functions
+
+(defun random-y-x (n k &optional 
+                   (x-rv (make-instance 'normal :sigma 9d0))
+                   (e-rv (make-instance 'normal :sigma 2d0))
+                   (beta (lla-vector k :double (generator* 'normal))))
+  "Generate random Y and X for testing regressions."
+  (bind ((x (make-matrix n k :double :initial-element (generator x-rv)))
+         (y (e+ (mm x beta)
+                (lla-vector n :double (generator e-rv)))))
+    (values y x)))
 
 ;;; linear regression with known variance
 
 (addtest (regressions-tests)
-  dummy-observations
+  dummy-observations-kv
   (bind ((mean (clo :double 2 3))
          (variance-right-sqrt (clo :double 
                                    5 7 :/
@@ -22,7 +33,57 @@
          (lr (linear-regression-kv y x)))
     (ensure-same (mean lr) mean)))
 
+(addtest (regressions-tests)
+  dummy-2phase-kv
+  ;; estimate in one and two steps, then compare
+  (bind ((k 10)
+         (n 100)
+         ((:values y x) (random-y-x (* 2 n) k))
+         (w (lla-vector (* 2 n) :double (generator* 'gamma))) ; weights
+         ;; single step
+         (p2 (linear-regression-kv y x :variance-right-sqrt (as-diagonal w)))
+         ;; two steps, first half
+         (h1 (cons 0 n))
+         (p1 (linear-regression-kv (sub y h1) (sub x h1 t)
+                                   :variance-right-sqrt 
+                                   (as-diagonal (sub w h1))))
+         ;; second half, using first half as prior
+         (h2 (cons n 0))
+         (p2-1 (linear-regression-kv (sub y h2) (sub x h2 t)
+                                     :prior p1
+                                     :variance-right-sqrt 
+                                     (as-diagonal (sub w h2)))))
+    (ensure-same (mean p2) (mean p2-1))
+    (ensure-same (variance p2) (variance p2-1))))
+
 ;;; linear regression
+
+(bind (((:values y x) (random-y-x 10 2))
+       (lr (linear-regression y x))
+       ((:values y-dummy x-dummy) (dummy-observations lr))
+       (lr2 (linear-regression y-dummy x-dummy)))
+  (d:v y-dummy x-dummy)
+  (d:v (mean lr) :/ (mean lr2))
+  (d:v (s^2 lr) :/ (s^2 lr2))
+  )
+
+(addtest (regressions-tests)
+  dummy-2phase
+  (bind ((k 2)
+         (n 10)
+         ((:values y x) (random-y-x (* 2 n) k))
+         ;; single step
+         (p2 (linear-regression y x))
+         ;; two steps, first half
+         (h1 (cons 0 n))
+         (p1 (linear-regression (sub y h1) (sub x h1 t)))
+         ;; second half, using first half as prior
+         (h2 (cons n 0))
+         (p2-1 (linear-regression (sub y h2) (sub x h2 t) :prior p1)))
+    (ensure-same (mean p2) (mean p2-1))
+    (ensure-same (variance p2) (variance p2-1))
+    (ensure-same (s^2 p2) (s^2 p2-1))
+    (ensure-same (nu p2) (nu p2-1))))
 
 (addtest (regressions-tests)
   linear-regression-exact
