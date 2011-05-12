256c3778ae0f1b3d9539b966266f776a00a4fed6
tpapp <tkpapp@gmail.com>
2011-05-12 15:25:19 +0200

    Added line break.

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,4 +1,5 @@
 (defpackage cl-random-tests
-  (:use cl alexandria iterate metabang-bind anaphora lift cl-random cl-num-utils lla)
+  (:use cl alexandria iterate metabang-bind anaphora lift cl-random
+        cl-num-utils lla)
   (:shadowing-import-from cl-random mean variance)
   (:export random-y-x run-cl-random-tests))
