a2e7c42c84c576a5b7d7a66ff34a461be36ec0d9
tpapp <tkpapp@gmail.com>
2013-02-12 12:55:14 +0100

    Reorganized test suite.
    Currently a skeleton, tests need to be rewritten and re-enabled.

--- cl-random.asd
+++ cl-random.asd
@@ -5,6 +5,7 @@
   :version "0.0.1"
   :depends-on (#:alexandria
                #:anaphora
+               #:array-operations
                #:cl-num-utils
                #:cl-rmath
                #:let-plus
@@ -30,31 +31,18 @@
   :description "Unit tests for CL-RANDOM."
   :author "Tamas K Papp <tkpapp@gmail.com"
   :license "Same as CL-RANDOM, this is part of the CL-RANDOM library."
+  :depends-on (#:cl-random
+               #:clunit)
+  :pathname #P"tests/"
   :serial t
   :components
-  ((:module
-    "package-init"
-    :pathname #P"tests/"
-    :components
-    ((:file "package")))
-   (:module
-    "utilities-and-setup"
-    :pathname #P"tests/"
-    :components
-    ((:file "utilities")
-     (:file "setup")))
-   (:module
-    "tests"
-    :pathname #P"tests/"
-    :components
-    ((:file "log-infinity")
-     (:file "special-functions")
-     (:file "discrete")
-     (:file "univariate")
-     (:file "continuous-time")
-     (:file "multivariate")
-     (:file "regressions")
-     (:file "statistics"))))
-  :depends-on
-  (#:cl-utilities #:iterate #:metabang-bind #:anaphora #:lift #:cl-num-utils
-                  #:cl-random #:lla #:cl-num-utils-tests))
+  ((:file "setup")
+   ;; (:file "log-infinity")
+   ;; (:file "special-functions")
+   ;; (:file "discrete")
+   ;; (:file "univariate")
+   ;; (:file "continuous-time")
+   ;; (:file "multivariate")
+   ;; (:file "regressions")
+   ;; (:file "statistics")
+))
--- tests/setup.lisp
+++ tests/setup.lisp
@@ -1,12 +1,71 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; Coding:utf-8 -*-
 
+(defpackage #:cl-random-tests
+  (:use #:cl
+        #:clunit
+        #:cl-num-utils.elementwise
+        #:cl-num-utils.matrix-shorthand
+        #:cl-random
+        #:let-plus
+        #:lla)
+  (:export #:run))
+
 (in-package #:cl-random-tests)
 
-(deftestsuite cl-random-tests () ()
-  (:equality-test #'==))
+(defsuite tests ())
 
-;; EXTERNAL
+(defun run (&optional interactive?)
+  "Run all the tests for CL-RANDOM."
+  (run-suite 'tests :use-debugger interactive?))
 
-(defun run ()
-  "Run all the tests for LLA."
-  (run-tests :suite 'cl-random-tests))
+
+(defun z-score (n mean variance sample-mean)
+  "Calculate abs(z) based on theoretical mean and variance and sample mean."
+  (abs (* (- sample-mean mean)
+          (sqrt (/ n variance)))))
+
+;; (defun same-sample-mean-variance (rv &key (n 100000) (z-band 4d0)
+;;                                           (var-band 0.1))
+;;   "Generate a sample of length N from RV, then use
+;; COMPARE-SAMPLE-MEAN-VARIANCE.  For univariate and multivariate distributions."
+;;   (let+ (((&accessors-r/o mean variance) rv))
+;;     (if (vectorp mean)
+;;         (let ((moments (clnu:central-sample-moments
+;;                         (aops:generate (funcall ))) )))
+;;         )
+
+;;     (multivariate? (vectorp mean))
+;;     (sample (sweep (if multivariate?
+;;                        (conforming-accumulator 'mean mean)
+;;                        'variance)
+;;                    (replicating rv n)))
+;;     (if multivariate?
+;;         (every (curry #'> z-band)
+;;                (map 'vector (curry #'z-score n) mean (diagonal variance)
+;;                     (mean sample)))
+;;         (and (< (z-score n mean variance (mean sample)) z-band)
+;;              (== variance (variance sample) var-band)))))
+
+(defgeneric relative-difference (a b)
+  (:documentation "Relative difference of A and B.")
+  (:method ((a number) (b number))
+    (/ (abs (- a b))
+       (max 1 (abs a) (abs b))))
+  (:method ((a array) (b array))
+    "Relative difference of A and B."
+    (assert (equalp (array-dimensions a) (array-dimensions b)))
+    (reduce #'max
+            (map 'vector #'relative-difference
+                 (aops:flatten a) (aops:flatten b))))
+  (:method ((a array) b)
+    (relative-difference a (aops:as-array b))))
+
+(defun random-y-x (n k
+                   &optional
+                   (x-rv (r-normal 0 9))
+                   (e-rv (r-normal 0 2))
+                   (beta (aops:generate* 'lla-double (generator (r-normal 0 1)) k)))
+  "Generate random Y and X for testing regressions."
+  (let* ((x (aops:generate* 'lla-double (generator x-rv) (list n k)))
+         (y (e+ (mm x beta) (aops:generate* 'lla-double n (generator e-rv)))))
+    (values y x)))
